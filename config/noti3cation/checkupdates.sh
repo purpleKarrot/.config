ce7edbe83cc3efda6774a0ac4a83c48a17470f1b
Daniel Pfeifer <daniel@pfeifer-mail.de>
2016-08-16 07:30:52 +0200

    cower

--- config/noti3cation/checkupdates.sh
+++ config/noti3cation/checkupdates.sh
@@ -4,3 +4,8 @@ updates=$(checkupdates)
 if [ -n "$updates" ]; then
     notify-send "System updates" "$updates"
 fi
+
+updates=$(cower -u)
+if [ -n "$updates" ]; then
+    notify-send "AUR updates" "$updates"
+fi
